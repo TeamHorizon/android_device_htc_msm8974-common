fcba4fde6c5bcb413b36f789244fc56352ddfcf6
Myles Watson <mylesgw@google.com>
2017-03-31 11:25:04 -0700

    msm8974-common: Add libbt-vendor to device.mk
    Bug: 36810308
    Test: build
    Change-Id: Iae4a6340a184dde818603b546470c8cf883f2f7b

--- msm8974-common.mk
+++ msm8974-common.mk
@@ -106,7 +106,8 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl
+    android.hardware.bluetooth@1.0-impl \
+    libbt-vendor
 
 # Camera shim
 PRODUCT_PACKAGES += \
